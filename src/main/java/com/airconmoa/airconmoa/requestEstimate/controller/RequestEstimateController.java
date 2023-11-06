06b7228f8af1c423e28fb79373b8fcf45c46822b
minjgziii <kmj4668@gmail.com>
2023-11-06 15:24:01 +0900

    REFACTOR #4 @Autowired 사용하지 않고 @RequiredArgsConstructor를 이용해 생성자 주입 형식을 사용하도록 변경

--- src/main/java/com/airconmoa/airconmoa/requestEstimate/controller/RequestEstimateController.java
+++ src/main/java/com/airconmoa/airconmoa/requestEstimate/controller/RequestEstimateController.java
@@ -2,6 +2,7 @@ package com.airconmoa.airconmoa.requestEstimate.controller;
 
 import com.airconmoa.airconmoa.domain.RequestEstimate;
 import com.airconmoa.airconmoa.requestEstimate.service.RequestEstimateService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +14,9 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/request_estimate")
+@RequiredArgsConstructor
 public class RequestEstimateController {
     private final RequestEstimateService requestEstimateService;
-
-    @Autowired
-    public RequestEstimateController(RequestEstimateService requestEstimateService) {
-        this.requestEstimateService = requestEstimateService;
-    }
 
     @PostMapping
     public ResponseEntity<RequestEstimate> createRequestEstimate(@RequestBody RequestEstimate requestEstimate) {
--- src/main/java/com/airconmoa/airconmoa/requestEstimate/service/RequestEstimateService.java
+++ src/main/java/com/airconmoa/airconmoa/requestEstimate/service/RequestEstimateService.java
@@ -3,17 +3,14 @@ package com.airconmoa.airconmoa.requestEstimate.service;
 import com.airconmoa.airconmoa.domain.RequestEstimate;
 import com.airconmoa.airconmoa.requestEstimate.repository.RequestEstimateRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RequestEstimateService {
     private final RequestEstimateRepository requestEstimateRepository;
-
-    @Autowired
-    public RequestEstimateService(RequestEstimateRepository requestEstimateRepository) {
-        this.requestEstimateRepository = requestEstimateRepository;
-    }
 
     @Transactional
     public RequestEstimate createRequestEstimate(RequestEstimate requestEstimate) {
