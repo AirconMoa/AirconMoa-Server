e4273a5b69235c50a084ff2427fa38426993aab7
minjgziii <kmj4668@gmail.com>
2023-11-08 18:42:42 +0900

    DEL #4 필요 없는 코드 제거

--- src/main/java/com/airconmoa/airconmoa/requestEstimate/service/RequestEstimateService.java
+++ src/main/java/com/airconmoa/airconmoa/requestEstimate/service/RequestEstimateService.java
@@ -4,7 +4,6 @@ import com.airconmoa.airconmoa.domain.RequestEstimate;
 import com.airconmoa.airconmoa.requestEstimate.repository.RequestEstimateRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
